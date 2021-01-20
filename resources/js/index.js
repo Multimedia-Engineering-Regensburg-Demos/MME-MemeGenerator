5841aaef2bcef76fc42c8e9ea1d993eda3846d7e
Alexander Bazo <alexanderbazo@googlemail.com>
2021-01-20 11:41:33 +0100

    Add defaults for canvas model

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,6 +1,9 @@
 /* eslint-env browser */
 
-const SEARCH_URL = "https://api.thecatapi.com/v1/images/search";
+const SEARCH_URL = "https://api.thecatapi.com/v1/images/search",
+    AVAILABLE_FONT_COLORS = ["#FFF", "#000"],
+    AVAILABLE_FONT_SIZES = ["14px", "24px", "36px", "48px", "72px", "96px", "144px"],
+    AVAILABLE_FONTS = ["Arial", "Verdana", "Helvetica", "Tahoma", "Trebuchet MS", "Times New Roman", "Georgia", "Garamond", "Courier New", "Brush Script MT"];
 
 function init() {
     console.log("### Cat-Meme-Generator ###");
